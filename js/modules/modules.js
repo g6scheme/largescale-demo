27bb15820f2ace0280331b5a89844fb8060531b8
Dusan Gledovic <dusan@gledovic.com>
2012-08-28 18:51:17 +0200

    some code styling, comments and fixes

--- js/modules/modules.js
+++ js/modules/modules.js
@@ -1,167 +1,162 @@
 (function(app){
     
-// Module for handling item counts
-app.core.define('#todo-counter', function(f){  
-    var counter = null, currentCount =0;
-    return {
-        init:function(){
-            counter = f.find('#count')[0];
-            f.subscribe({
-                'entry-validated' : this.updateCounter
-            });
+    // Module for handling item counts
+    app.core.define('#todo-counter', function(f){  
+        var counter = null, currentCount =0;
+        return {
+            init:function(){
+                counter = f.find('#count')[0];
+                f.subscribe({
+                    'entry-validated' : this.updateCounter
+                });
 
-        },
-        destroy:function(){
-            counter = null;
-            currentCount = 0;
-        },
+            },
+            destroy:function(){
+                counter = null;
+                currentCount = 0;
+            },
 
-        updateCounter:function(){
-            currentCount++;
-            f.setHTML(counter,currentCount);
-            f.publish({
-                type : 'counter-updated'
-            });
-        }
-    } 
-});
+            updateCounter:function(){
+                currentCount++;
+                f.setHTML(counter,currentCount);
+                f.publish({
+                    type : 'counter-updated'
+                });
+            }
+        } 
+    });
 
+    // Module for handling clean-up post entry being added
+    app.core.define('#todo-field', function(f){
+        var input;
+        return {
+            init:function(){
+                var that = this;
+                input = f.getEl();
 
-// Module for handling clean-up post entry being added
-app.core.define('#todo-field', function(f){
-    var input;
-    return {
-        init:function(){
-            var that = this;
-            input = f.getEl();
+                f.subscribe({
+                    'entry-validated' : this.clearEntry       
+                });
+            },
+            destroy:function(){
+            
+            },
+            clearEntry:function(){
+                input.value = "";
+            }
+        };  
+    });
 
-            f.subscribe({
-                'entry-validated' : this.clearEntry       
-            });
-        },
-        destroy:function(){
-        
-        },
-        clearEntry:function(){
-            input.value = "";
-        }
-    };  
-});
+    // Module for handling the detection and publication of new entries
+    app.core.define('#todo-entry', function(f){
+        var input, button;
+        return {
+            init: function (){
 
-// Module for handling the detection and publication of new entries
-app.core.define('#todo-entry', function(f){
-    var input, button;
-    return {
-        init: function (){
+                input = f.find('input')[0],
+                button = f.find('button')[0];
 
-            input = f.find('input')[0],
-            button = f.find('button')[0];
+                f.bind(button, 'click', this.handleEntry);
+                f.bind(input, 'keydown', this.handleKey);
 
-            f.bind(button, 'click', this.handleEntry);
-            f.bind(input, 'keydown', this.handleKey);
+            },
 
-        },
+            destroy: function(){
+                f.unbind(button, "click", this.handleEntry);
+                f.unbind(input, "keydown", this.handleKey);
+                input = button = reset = null;     
+            },
 
-        destroy: function(){
-            f.unbind(button, "click", this.handleEntry);
-            f.unbind(input, "keydown", this.handleKey);
-            input = button = reset = null;     
-        },
-
-        handleEntry: function (){
-            f.publish({
-                type : 'new-entry',
-                data : {value: input.value, id: f.newGUID()}
-            });
-        },
-
-        handleKey: function(e){
-            if(e.which == 13){
+            handleEntry: function (){
                 f.publish({
                     type : 'new-entry',
                     data : {value: input.value, id: f.newGUID()}
                 });
+            },
 
+            handleKey: function(e){
+                if(e.which == 13){
+                    f.publish({
+                        type : 'new-entry',
+                        data : {value: input.value, id: f.newGUID()}
+                    });
+
+                }
             }
-        }
         }
     });
 
-
-
-app.core.define('#validate', function(f){
-    return {
-        init: function(){
-            f.subscribe({
-                'new-entry' : this.validateEntry    
-            });
-        },
-
-        destroy: function(){
-        },
-
-        validateEntry: function ( todoItem ){
-            if(todoItem.value == ""){
-               f.publish({
-                    type : 'input-error',
-                    data : {value: 'Please ensure your input is valid'}
+    app.core.define('#validate', function(f){
+        return {
+            init: function(){
+                f.subscribe({
+                    'new-entry' : this.validateEntry    
                 });
-            }else{
-                 f.publish({
-                    type : 'entry-validated',
-                    data : todoItem
-                });
+            },
+
+            destroy: function(){
+            },
+
+            validateEntry: function ( todoItem ){
+                if(todoItem.value == ""){
+                   f.publish({
+                        type : 'input-error',
+                        data : {value: 'Please ensure your input is valid'}
+                    });
+                }else{
+                     f.publish({
+                        type : 'entry-validated',
+                        data : todoItem
+                    });
+                }
             }
-        }
-    }    
-});
+        }    
+    });
 
+    app.core.define('#error', function(f){
+        var errorNotice;
+        
+        return {
+            init: function(){
+                errorNotice = f.find('#alert')[0];
 
-app.core.define('#error', function(f){
-    var errorNotice;
-    
-    return {
-        init: function(){
-            errorNotice = f.find('#alert')[0];
+                f.subscribe({
+                    'input-error' : this.showError        
+                });
 
-            f.subscribe({
-                'input-error' : this.showError        
-            });
+            },
 
-        },
+            destroy: function(){
+                errorNotice = null;
+            },
 
-        destroy: function(){
-            errorNotice = null;
-        },
+            showError: function( msg ){
+                f.setHTML(errorNotice, msg.value);
+            }
+        }    
+    });
 
-        showError: function( msg ){
-            f.setHTML(errorNotice, msg.value);
-        }
-    }    
-});
+    app.core.define("#todo-list", function (f) {
+        var todo, todoItems, renderedItems;
+        
+        return {
+            init : function () {
+                todo = f.find("ul")[0];
+                todoItems = {};
 
+                f.subscribe({
+                    'entry-validated' : this.addItem        
+                });
 
-app.core.define("#todo-list", function (f) {
-    var todo, todoItems, renderedItems;
-    
-    return {
-        init : function () {
-            todo = f.find("ul")[0];
-            todoItems = {};
+            },
 
-            f.subscribe({
-                'entry-validated' : this.addItem        
-            });
+            destroy : function () {
+                todo = todoItems = null;
+                f.ignore(['add-item']);
+            },
 
-        },
-
-        destroy : function () {
-            todo = todoItems = null;
-            f.ignore(['add-item']);
-        },
-
-        addItem : function ( todoItem ) {
-            var entry; 
+            addItem : function ( todoItem ) {
+                var entry; 
 
                 entry = f.createElement("li", { 
                     id : "todo-" + todoItem.id, 
@@ -177,14 +172,11 @@ app.core.define("#todo-list", function (f) {
                 f.css(entry, {'color': f.getRandomColor(), 'background': f.getRandomColor()});
                 f.animate(entry, {'line-height':'50'}, 500);
                 todoItems[todoItem.id] = 1;
-            
-        }
-    };
-});
+                
+            }
+        };
+    });
 
-
-
-app.core.startAll();
-
+    app.core.startAll();
 
 })(app);
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,58 +1,53 @@
-(function(app){
-	
-	app.extendObj = function (destination, source) {
-	    var toString = Object.prototype.toString,
-	        toStringCall = toString.call({});
-	    for (var property in source) {
-	        if (source[property] && toStringCall == toString.call(source[property])) {
-	            destination[property] = destination[property] || {};
-	            app.extendObj(destination[property], source[property]);
-	        } else {
-	            destination[property] = source[property];
-	        }
-	    }
-	    return destination;
-	}
+(function(app) {
 
-	app.extend = function(source){
-	    if(typeof source =="string"){
-	        app[source] = {}
-	    }else{
-	    app.extendObj(app, source);
-	}
-	}
+    app.extendObj = function(destination, source) {
+        var toString = Object.prototype.toString,
+            toStringCall = toString.call({});
+        for (var property in source) {
+            if (source[property] && toStringCall == toString.call(source[property])) {
+                destination[property] = destination[property] || {};
+                app.extendObj(destination[property], source[property]);
+            } else {
+                destination[property] = source[property];
+            }
+        }
+        return destination;
+    }
+    app.extend = function(source) {
+        if (typeof source == "string") {
+            app[source] = {}
+        } else {
+            app.extendObj(app, source);
+        }
+    }
 
+    app.utils = (function() {
+        return {
+            typeEqual: function(input, desiredType) {
+                return app.utils.type(input).toLowerCase() == desiredType;
+            },
+            type: function(input) {
+                return Object.prototype.toString.call(input).match(/^\[object\s(.*)\]$/)[1];
+            },
 
-	app.utils = (function(){
-	  return{
-	            typeEqual: function(input, desiredType){
-	          return app.utils.type(input).toLowerCase() == desiredType;
-	      },
+            newGUID: function() {
 
-	      type:function(input){
-	        return Object.prototype.toString.call(input).match(/^\[object\s(.*)\]$/)[1];
-	    },
+                var S4 = function() {
+                        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                    };
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            }
+        }
+    }());
 
-	    newGUID: function(){
-
-	      var S4 = function() {
-	         return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-	      };
-	      return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-
-	    }
-
-	}
-	}());
-
-
-	app.randomColor = function() {
-	    var letters = '0123456789ABCDEF'.split(''),
-	         color = '#',i=0;
-	    for (i = 0; i < 6; i++ ) {
-	        color += letters[Math.round(Math.random() * 15)];
-	    }
-	    return color;
-	}
+    app.randomColor = function() {
+        var letters = '0123456789ABCDEF'.split(''),
+            color = '#',
+            i = 0;
+        for (i = 0; i < 6; i++) {
+            color += letters[Math.round(Math.random() * 15)];
+        }
+        return color;
+    }
 
 })(app);
